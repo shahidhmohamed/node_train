f03cdb35beb8f9fc71b1d8c6290181196db2db5c
shahidhmohamed <mohamedshahidh81@gmail.com>
2024-09-26 09:59:57 +0530

    Page Creation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 // import Form from "./components/form";
 // import TodoList from "./components/todoList";
-import Product from "./components/product";
-import TodoPage from "./components/todoApp";
+import Product from "./pages/product/product";
+import TodoPage from "./pages/todo/todoApp";
 import AppScreen from "./components/app";
 
 function App() {
